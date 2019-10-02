ab890e77d35446073b056f9bea4d3770df10d788
Dayanara <56050496+BenitezDI@users.noreply.github.com>
2019-10-02 15:04:37 -0700

    Update dice.py

--- dice.py
+++ dice.py
@@ -13,3 +13,18 @@ rolls = random.random(1,6)
 
 print("Hello human being of this earth. Welcome tooooooo Dice Rolling Simulator!")
 roll = imput (" How many time woud like to roll the dice?: ") 
+
+While True:
+  if rolls == "1":
+    side1 += 1
+  elif roll == "2":
+    side2 += 
+  elif roll == "3":
+    side3 += 1
+   elif roll == "4":
+    side4 += 1 
+   elif roll == "5":
+    side5 += 1
+   else = 6
+    
+    
