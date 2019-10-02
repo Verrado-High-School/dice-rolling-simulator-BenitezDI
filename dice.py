44423fb28d85de25574582bbe8919889fb18b32a
Dayanara <56050496+BenitezDI@users.noreply.github.com>
2019-10-02 14:53:10 -0700

    Update dice.py

--- dice.py
+++ dice.py
@@ -1,3 +1,15 @@
 # Name: Dayanara 
 # Period 6 
 # Dice Rolling Simulator
+
+import random 
+
+side1 = 0 
+side2 = 0
+side3 = 0
+sied4 = 0 
+side5 = 0 
+rolls = random.random(1,6)
+
+print("Hello human being of this earth. Welcome tooooooo Dice Rolling Simulator!")
+roll = imput (" How many time woud like to roll the dice?: ") 
